6c097d4c548309da3f7d230413136c8937ecd825
Yoel <yoelashkenazi12@gmail.com>
2024-07-24 12:09:29 +0300

    updated results

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+import numpy as np
+
 import statistic_analysis as sa
 from typing import List
 import pandas as pd
+import warnings
+warnings.filterwarnings("ignore")
 
 
 def check_multiple_cols(data: pd.DataFrame, columns: List[str], function: callable, **kwargs):
@@ -28,17 +32,57 @@ if __name__ == '__main__':
 
     # set up parameters
     ALPHA = 0.05
-    function_args = {'alpha': 0.05, "print_results": False, 'plot': True}
+    function_args = {'alpha': 0.05, "print_results": True, 'plot': True, 'model_name': 'xgb'}
 
     df = sa.load_data()  # Load data
 
     # remember important columns
     important_cols = ["InterviewScore", "SkillScore", "PersonalityScore"]
+    # regular columns
+    regular_cols = ["Age", "Gender", "EducationLevel", "ExperienceYears", "PreviousCompanies"]
 
     target_col = "HiringDecision"
+
+    # plot PCA
+    # sa.plot_pca(df, 2)
+    # sa.plot_pca(df, 3)
 
     # check the distribution of the important columns
     # check_multiple_cols(df, important_cols, sa.analyze_distribution, **function_args)
 
     # check the dependence of the important columns on the target column
-    check_multiple_cols(df, important_cols, sa.check_dependence, **function_args)
+    # check_multiple_cols(df, important_cols, sa.check_dependence, **function_args)
+
+    # check the hypothesis with the xgboost model
+    xgb_differences = []
+    for i, feature in enumerate(regular_cols):
+        print(f"Testing the theory of {feature} improvement with {function_args['model_name']}:")
+        a_, b = sa.test_theory_feature_improvement(df, feature, **function_args)
+        xgb_differences.append(list(np.array(b) - np.array(a_)))  # get the differences
+        print('-'*40)
+
+    # check the hypothesis with the neural network
+    nn_diff = []
+    function_args['model_name'] = 'nn'
+    for i, feature in enumerate(regular_cols):
+        print(f"Testing the theory of {feature} improvement with {function_args['model_name']}:")
+        a_, b = sa.test_theory_feature_improvement(df, feature, **function_args)
+        nn_diff.append(list(np.array(b) - np.array(a_)))  # get the differences
+        print('-'*40)
+
+    # perform the ANOVA test
+    print("ANOVA test for XGB:")
+    sa.test_theory_contribution(xgb_differences, **function_args)
+    print('-' * 100)
+    print("ANOVA test for NN:")
+    sa.test_theory_contribution(nn_diff, **function_args)
+
+    # check the biases.
+    # for col in regular_cols:
+    #     print(f"\nChecking the bias of {col} on the target column...")
+    #     show_rates = True if col != "Age" else False
+    #     sa.plot_feature_biases(df, col, show_rates)
+    #     print('-'*40)
+
+# todo: show some biases between the values of each categorical feature's values and the target column.
+# todo: check the same results for other features (compare the accuracy of the model with and without the feature)
--- statistic_analysis.py
+++ statistic_analysis.py
@@ -1,9 +1,14 @@
+import random
+
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 from scipy import stats
+from scipy.interpolate import make_interp_spline
 from sklearn.decomposition import PCA
+from sklearn.model_selection import train_test_split
+from typing import List
 
 # Constants
 BLUE, RED = '#5F4B8B', '#E69A8D'
@@ -34,6 +39,9 @@ def plot_pca(data: pd.DataFrame, n_components: int = 2):
     # Fit and transform the data.
     principal_components = pca.fit_transform(data.iloc[:, : -1].copy())
 
+    # calculate the explained variance ratio
+    explained_variance_ratio = np.sum(pca.explained_variance_ratio_)
+
     # Create a new dataframe with the principal components.
     principal_df = pd.DataFrame(data=principal_components)
 
@@ -51,7 +59,7 @@ def plot_pca(data: pd.DataFrame, n_components: int = 2):
         plt.grid()  # Add grid
         plt.xlabel('Principal Component 1')  # Add x label
         plt.ylabel('Principal Component 2')  # Add y label
-        plt.title('PCA 2D')  # Add title
+        plt.title(f'PCA 2D \nExplained Variance: {(explained_variance_ratio * 100):.3f}%')  # Add title
 
         # Create legend handles
         handles = [plt.Line2D(['Reject'], ['Accept'], marker='o', color='w', markerfacecolor=col,
@@ -73,7 +81,7 @@ def plot_pca(data: pd.DataFrame, n_components: int = 2):
         ax.set_xlabel('Principal Component 1')  # Add x label
         ax.set_ylabel('Principal Component 2')  # Add y label
         ax.set_zlabel('Principal Component 3')  # Add z label
-        plt.title('PCA 3D')  # Add title
+        plt.title(f'PCA 3D \nExplained Variance: {(explained_variance_ratio * 100):.3f}%')  # Add title
 
         # Create legend handles
         handles = [plt.Line2D(['Reject'], ['Accept'], marker='o', color='w', markerfacecolor=col,
@@ -151,7 +159,6 @@ def analyze_distribution(data_: pd.DataFrame, feature_: str, **kwargs):
         axs[0].legend()  # Add legend
         axs[0].set_xlabel(feature_)  # Add x label
         axs[0].set_ylabel('Density')  # Add y label
-        axs[0].grid()  # Add grid
 
         # Plot the feature distribution vs uniform distribution
         sns.kdeplot(feature, color=BLUE, ax=axs[1], label=feature_)
@@ -165,7 +172,6 @@ def analyze_distribution(data_: pd.DataFrame, feature_: str, **kwargs):
         axs[1].legend()  # Add legend
         axs[1].set_xlabel(feature_)  # Add x label
         axs[1].set_ylabel('Density')  # Add y label
-        axs[1].grid()  # Add grid
 
         # save the plot
         plt.savefig(f'Analysis Plots/Distribution Plots/{feature_}.png')
@@ -223,3 +229,272 @@ def check_dependence(data: pd.DataFrame, feature1: str, **kwargs):
 
     return pd.DataFrame({'Chi-Square Statistic': chi_stats, 'P-Value': p_vals,
                          'Dependent': dependence})
+
+
+def test_theory_feature_improvement(data: pd.DataFrame, feature: str, **kwargs):
+    """
+    Args:
+    data: the data to check
+    feature: the feature to check
+    kwargs: the keyword arguments
+    Returns:
+    the results of the model with and without the feature.
+    """
+
+    # Constants
+    ALPHA = kwargs.get('alpha', 0.05)  # Get the alpha value
+    print_results = kwargs.get('print_results', True)  # Get the print_results value
+    model_name = kwargs.get('model_name', 'xgb')  # Get the model name
+    plot_results = kwargs.get('plot', True)  # Get the plot_results value
+
+    assert model_name in ['xgb', 'nn'], "model_name must be either 'xgb' or 'nn'"
+
+    """
+    Step 1: divide the data into train and test sets, and keep a copy of the data without the feature.
+    """
+    # Create a copy of the data without the feature
+    X = data.iloc[:, :-1].copy()
+    y = data.iloc[:, -1].copy()  # Get the target variable
+
+    # Create a copy of the data without the feature
+    X_no_feature = X.copy()
+    X_no_feature.drop(columns=[feature], inplace=True)
+
+    shuffle_state = random.choice(range(1000))  # Get a random shuffle state
+
+    x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=shuffle_state)
+
+    x_train_no_feature, x_test_no_feature, _, _ = train_test_split(X_no_feature, y, test_size=0.2,
+                                                                   random_state=shuffle_state)
+
+    """
+    Step 2: train the model with and without the feature
+    """
+    if model_name == 'xgb':
+        from xgboost import XGBClassifier
+
+        # Train the model with the feature
+        model_with_feature = XGBClassifier(use_label_encoder=False)
+        model_with_feature.fit(x_train, y_train)
+
+        # Train the model without the feature
+        model_without_feature = XGBClassifier(use_label_encoder=False)
+        model_without_feature.fit(x_train_no_feature, y_train)
+
+    else:
+        from sklearn.neural_network import MLPClassifier
+
+        # Train the model with the feature
+        model_with_feature = MLPClassifier()
+        model_with_feature.fit(x_train, y_train)
+
+        # Train the model without the feature
+        model_without_feature = MLPClassifier()
+        model_without_feature.fit(x_train_no_feature, y_train)
+
+    """
+    Step 3: evaluate the models
+    """
+
+    # divide the test data (300 samples) into 10 batches of 30, and calculate the mean accuracy of each batch.
+    batch_size = 30
+    n_batches = 10
+    accuracies_with_feature = []
+    accuracies_without_feature = []
+
+    for i in range(n_batches):
+        start = i * batch_size
+        end = (i + 1) * batch_size
+
+        # Evaluate the model with the feature
+        score_with = model_with_feature.score(x_test.iloc[start:end], y_test.iloc[start:end])
+        accuracies_with_feature.append(score_with)
+
+        # Evaluate the model without the feature
+        score_without = model_without_feature.score(x_test_no_feature.iloc[start:end], y_test.iloc[start:end])
+        accuracies_without_feature.append(score_without)
+
+    if plot_results:
+        # Plot the accuracies as a curve
+        plt.figure()
+        accs_with = np.array(accuracies_with_feature)
+        accs_without = np.array(accuracies_without_feature)
+        x_vals = np.linspace(0, n_batches - 1, 300)
+        spl = make_interp_spline(range(n_batches), accs_with, k=3)
+        accs_with = spl(x_vals)
+        sns.lineplot(x=x_vals, y=accs_with, color=BLUE, label='With Feature')
+
+        spl = make_interp_spline(range(n_batches), accs_without, k=3)
+        accs_without = spl(x_vals)
+        sns.lineplot(x=x_vals, y=accs_without, color=RED, label='Without Feature')
+        plt.xlabel('Batch number')
+        plt.ylabel('Accuracy')
+        plt.legend()
+        plt.grid()
+        plt.title(f"{'XGB' if model_name == 'xgb' else 'NN'} Accuracy with and without '{feature}\n"
+                  f"Mean Accuracy with '{feature}': {np.mean(accs_with):.3f}\n"
+                  f"Mean Accuracy without '{feature}': {np.mean(accs_without):.3f}")
+        try:
+            plt.savefig(f'Analysis Plots/Accuracy Plots/{model_name}_{feature}_accuracy.png')
+        except FileNotFoundError:
+            import os
+            os.mkdir('Analysis Plots/Accuracy Plots')
+        plt.savefig(f'Analysis Plots/Accuracy Plots/{model_name}_{feature}_accuracy.png')
+        plt.show()
+
+    """
+    Now we have 2 vectors of 10 accuracies each, one for the model with the feature and one for the model without 
+    the feature. Now we apply the statistical analysis to check if the feature improves the accuracy.
+    """
+
+    # Shapiro-Wilk test for normality of the differences
+    _, p_shapiro = stats.shapiro(np.array(accuracies_with_feature) - np.array(accuracies_without_feature))
+    print(f"Normality test for '{feature}' (Shapiro-Wilk):")
+    print(f"p-value: {p_shapiro:.5f}", end='')
+
+    if p_shapiro > ALPHA:
+        print(f" The differences are normally distributed for '{feature}'.")
+
+        # Perform the paired t-test
+        t_stat, p_val = stats.ttest_rel(accuracies_with_feature, accuracies_without_feature, alternative='greater')
+
+        # Print the results
+        if print_results:
+            print(f"Paired T-Test for '{feature}':")
+            print(f"T-Statistic: {t_stat:.5f}")
+            print(f"P-Value: {p_val:.5f}")
+            if p_val < ALPHA:
+                print(f"The feature '{feature}' does not improve the accuracy of the model.")
+            else:
+                print(f"The feature '{feature}' improves the accuracy of the model.")
+
+    else:
+        print(f" The differences are not normally distributed for '{feature}'.")
+
+        # Perform the Wilcoxon signed-rank test
+        diff = np.array(accuracies_with_feature) - np.array(accuracies_without_feature)
+        t_stat, p_val = stats.wilcoxon(diff, alternative='greater')
+
+        # Print the results
+        if print_results:
+            print(f"Wilcoxon Signed-Rank Test for '{feature}':")
+            print(f"Statistic: {t_stat:.5f}")
+            print(f"P-Value: {p_val:.5f}")
+            if p_val < ALPHA:  # If the p-value is less than 0.05
+                print(f"The feature '{feature}' does not improve the accuracy of the model.")
+            else:
+                print(f"The feature '{feature}' improves the accuracy of the model.")
+
+    return accuracies_with_feature, accuracies_without_feature
+
+
+def test_theory_contribution(differences_per_feature: List, **kwargs):
+    """
+    Args:
+    differences_per_feature: List of lists of accuracy difference with and without the feature.
+    kwargs: the keyword arguments
+
+    This method performs the ANOVA test to check if the differences in accuracy are significant.
+
+    Returns:
+    The results of the ANOVA test.
+    """
+
+    # Constants
+    ALPHA = kwargs.get('alpha', 0.05)  # Get the alpha value
+    print_results = kwargs.get('print_results', True)  # Get the print_results value
+
+    """
+    We know that each sample is normally distributed, we need to check for equality of variances.
+    We'll use the Levene test for this.
+    """
+
+    # Perform the Levene test
+    stat, p_val = stats.levene(*differences_per_feature)
+
+    # Print the results
+    if print_results:
+        print("Levene Test for Equality of Variances:")
+        print(f"Statistic: {stat:.5f}")
+        print(f"P-Value: {p_val:.5f}")
+        if p_val < ALPHA:
+            print("The variances are not equal.")
+        else:
+            print("The variances are equal.")
+
+    # Perform the ANOVA test if the variances are equal
+    if p_val > ALPHA:
+        f_stat, p_val = stats.f_oneway(*differences_per_feature)
+
+        # Print the results
+        if print_results:
+            print("ANOVA Test:")
+            print(f"F-Statistic: {f_stat:.5f}")
+            print(f"P-Value: {p_val:.5f}")
+            if p_val < ALPHA:  # If the p-value is less than 0.05 can't reject the null hypothesis
+                print("The features contributed equally.")
+            else:
+                print("The features did not contribute equally.")
+    else:
+        print("Cannot perform ANOVA test because the variances are not equal.")
+        # Perform the Kruskal-Wallis test if the variances are not equal
+        stat, p_val = stats.kruskal(*differences_per_feature)
+
+        # Print the results
+        if print_results:
+            print("Kruskal-Wallis Test:")
+            print(f"Statistic: {stat:.5f}")
+            print(f"P-Value: {p_val:.5f}")
+            if p_val < ALPHA:
+                print("The features contributed equally.")
+            else:
+                print("The features did not contribute equally.")
+
+
+def plot_feature_biases(data: pd.DataFrame, feature: str, show_rates: bool = True):
+    """
+    Args:
+        data: the data to check
+        feature: the feature to check
+        show_rates: whether to show the relative acceptance and rejection rates on the plot.
+
+    This method plots the biases of the model with and without the feature.
+
+    Returns:
+    None
+    """
+
+    # for each value in the feature, find how many times the label was accepted and rejected
+    feature_values = data[feature].unique()
+    acceptances = []
+    rejections = []
+    for val in feature_values:
+        acceptances.append(data[(data[feature] == val) & (data['HiringDecision'] == 1)].shape[0])
+        rejections.append(data[(data[feature] == val) & (data['HiringDecision'] == 0)].shape[0])
+
+    # find the relative acceptances and rejections for each unique value of the feature
+    relative_acceptances = [acceptances[i] / (acceptances[i] + rejections[i]) for i in range(len(acceptances))]
+    relative_rejections = [rejections[i] / (acceptances[i] + rejections[i]) for i in range(len(rejections))]
+    # plot the biases, and print the relative acceptance and rejections on the plot.
+    plt.figure()
+    plt.bar(feature_values, acceptances, color=BLUE, label='Acceptances')
+    plt.bar(feature_values, rejections, color=RED, label='Rejections', bottom=acceptances)
+    # add the text to the plot
+    if show_rates:
+        for i, val in enumerate(feature_values):
+            plt.text(val, acceptances[i] / 2, f'{relative_acceptances[i]:.2f}', ha='center', va='center')
+            plt.text(val, acceptances[i] + rejections[i] / 2, f'{relative_rejections[i]:.2f}',
+                     ha='center', va='center')
+    plt.xlabel(feature)
+    plt.ylabel('Relative Frequency')
+    plt.title(f"Biases in Hiring Decision given '{feature}'")
+    plt.legend()
+
+    # save the figure
+    try:
+        plt.savefig(f'Analysis Plots/Bias Plots/{feature}_biases.png')
+    except OSError:
+        import os
+        os.mkdir('Analysis Plots/Bias Plots')
+        plt.savefig(f'Analysis Plots/Bias Plots/{feature}_biases.png')
+    plt.show()
